5faa8bb62ed915a94d9c728ea2c493401f794fcb
byObin <96936141+byObin@users.noreply.github.com>
2024-01-21 12:22:50 +0800

    [level 0] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,11 +1,12 @@
 def solution(num_list):
-    answer = 0
+    list_sum = 0
     mul_sum = 1
     
     for i in num_list:
+        list_sum += i
         mul_sum *= i
     
-    if mul_sum < (sum(num_list)**2):
+    if mul_sum < list_sum**2 :
         return 1
     else:
         return 0
