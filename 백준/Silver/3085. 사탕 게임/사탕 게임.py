54df2559c0836a47bb13e9e2d74a80170d7d8ce3
byObin <byobin26@gmail.com>
2024-02-22 16:06:23 +0900

    Update 사탕 게임.py

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,43 +1,50 @@
-def check(data):
-    max_cnt = 1
-    for i in range(N):
+n = int(input())
+
+candy = [list(input()) for _ in range(n)]
+
+def candy_cnt(candy):
+    max_cnt = 1  
+    for k in range(0,n):
         cnt = 1
-        for j in range(1, N):
-            if data[i][j] == data[i][j-1]:
+        for l in range(1,n):
+            if candy[k][l] == candy[k][l-1]:
+                cnt += 1
+            else:
+                cnt = 1
+            max_cnt = max(max_cnt, cnt)
+        
+        cnt = 1
+        for l in range(1,n):
+            if candy[l][k] == candy[l-1][k] :
                 cnt += 1
             else:
                 cnt = 1
             max_cnt = max(max_cnt, cnt)
 
-        cnt = 1
-        for j in range(1, N):
-            if data[j][i] == data[j-1][i]:
-                cnt += 1
-            else:
-                cnt = 1
-            max_cnt = max(max_cnt, cnt)
-    
     return max_cnt
 
-N = int(input())
-data = [list(input()) for _ in range(N)]
-ans = 0
 
-for i in range(N):
-    for j in range(N):
-        if j + 1 < N: # 열 범위 체크
+
+answer = 0
+
+
+for i in range(n):
+    for j in range(n):
+        if j + 1 < n: # 열 범위 체크
             # 인접한 것끼리 바꿔주기
-            data[i][j], data[i][j+1] = data[i][j+1], data[i][j]
-            cnt = check(data)
-            ans = max(ans, cnt)
+            candy[i][j], candy[i][j+1] = candy[i][j+1], candy[i][j]
+            cnt = candy_cnt(candy)
+            answer = max(answer, cnt)
             # 바꾼 것 원래대로 돌려놓기
-            data[i][j], data[i][j+1] = data[i][j+1], data[i][j]
+            candy[i][j], candy[i][j+1] = candy[i][j+1], candy[i][j]
 
-        if i + 1 < N: # 행 범위 체크
-            data[i][j], data[i+1][j] = data[i+1][j], data[i][j]
-            cnt = check(data)
-            ans = max(ans, cnt)
+        if i + 1 < n: # 행 범위 체크
+            candy[i][j], candy[i+1][j] = candy[i+1][j], candy[i][j]
+            cnt = candy_cnt(candy)
+            answer = max(answer, cnt)
             # 바꾼 것 원래대로 돌려놓기
-            data[i][j], data[i+1][j] = data[i+1][j], data[i][j]
+            candy[i][j], candy[i+1][j] = candy[i+1][j], candy[i][j]
 
-print(ans)
+
+
+print(answer)
