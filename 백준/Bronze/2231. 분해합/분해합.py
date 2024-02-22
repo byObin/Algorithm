b8acac750dc9ffbe6e47924310fa74ef76e912f2
byObin <byobin26@gmail.com>
2024-02-22 17:14:43 +0900

    분해합 각 자릿수의 합 sum(map(int,str(i))

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,18 +1,10 @@
 n = int(input())
-exist = False
 
-def sum_of_each_numbers(num):
-    numbers = []
-    while num >= 1:
-        numbers.append(num%10)
-        num  = num // 10
-    return sum(numbers)
+answer = 0
 
 for i in range(1,n+1):
-    if sum_of_each_numbers(i)+i == n:
-        print(i)
-        exist = True
+    if sum(map(int, str(i)))+i == n:
+        answer = i
         break
 
-if exist == False:
-    print(0)
+print(answer)
