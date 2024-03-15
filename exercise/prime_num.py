06e60b66c9acba1a8ba086283f629af0eff36bcd
byObin <byobin26@gmail.com>
2024-03-15 17:51:59 +0900

    알고리즘 개선 1
    2의 배수 판단 x
    이미 찾은 소수로만 나눗셈 하기
    통해 나눗셈 실행 횟수 78,022 -> 14,622로 감소

--- exercise/prime_num.py
+++ exercise/prime_num.py
@@ -1,13 +1,24 @@
 # n 이하의 소수 나열하기
 
-count = 0
+count = 0       # 나눗셈 한 횟수
+ptr = 0         # 이미 찾은 소수의 개수
+
 n = int(input('n을 입력하세요 : '))
-for i in range(2,n+1):
-    for j in range(2,i):
+
+prime = [None]*(n//2)   # 찾은 소수 저장하는 배열
+prime[ptr] = 2          # 2는 반드시 가장 작은 소수이므로 초깃값 지정
+ptr += 1
+
+for i in range(3, n+1, 2):    # 2의 배수는 소수가 아니므로 홀수만 탐색
+    for j in range(1, ptr):  # 이미 찾은 소수로 나눗셈
         count += 1 
-        if i%j == 0:    # 나누어 떨어지면 소수 아님
+        if i % prime[j] == 0:    # 나누어 떨어지면 소수 아님
             break
-    else:               # 끝까지 나누어 떨어지면 소수임
-        print(i)
+    else:               # 끝까지 나누어 떨어지지 않으면 소수임
+        prime[ptr] = i  # 소수이므로 prime배열에 입력
+        ptr += 1
+
+for i in range(ptr):
+    print(prime[i])
 
 print(f'나눗셈 한 횟수 : {count}')
