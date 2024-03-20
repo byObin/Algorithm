a1e412c0777b1518df3dcff25f62500cac9dbb55
byObin <byobin26@gmail.com>
2024-03-20 10:18:15 +0900

    알고리즘 개선 2
    n의 제곱근 이하의 어떤 소수로도 나누어떨어지지 않으면 소수라고 할 수 있다는 성질을 이용하여 연산 횟수 단축

--- exercise/prime_num.py
+++ exercise/prime_num.py
@@ -9,14 +9,20 @@ prime = [None]*(n//2)   # 찾은 소수 저장하는 배열
 prime[ptr] = 2          # 2는 반드시 가장 작은 소수이므로 초깃값 지정
 ptr += 1
 
-for i in range(3, n+1, 2):    # 2의 배수는 소수가 아니므로 홀수만 탐색
-    for j in range(1, ptr):  # 이미 찾은 소수로 나눗셈
-        count += 1 
-        if i % prime[j] == 0:    # 나누어 떨어지면 소수 아님
+prime[ptr] = 3
+ptr += 1
+
+for i in range(5, n+1, 2):    # 2의 배수는 소수가 아니므로 홀수만 탐색
+    n = 1
+    while prime[n] * prime[n] <= i:
+        count += 2
+        if i % prime[n] == 0:   # 나누어 떨어지면 소수 아님
             break
+        n += 1
     else:               # 끝까지 나누어 떨어지지 않으면 소수임
         prime[ptr] = i  # 소수이므로 prime배열에 입력
         ptr += 1
+        count += 1
 
 for i in range(ptr):
     print(prime[i])
