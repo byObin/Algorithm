427def1489ff8ecddd34085d89b7368f3ea17acc
byObin <96936141+byObin@users.noreply.github.com>
2024-04-03 22:50:05 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 348 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,29 @@
+
+while True:
+    stack = []
+   
+    line = input()
+
+    if line == '.':
+        break    
+    
+    for i in line:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) > 0 and stack[-1] == '(':
+                    stack.pop()
+            else:
+                stack.append(i)
+                break
+        elif i == ']':
+            if len(stack) > 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(i) 
+                break
+    
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
