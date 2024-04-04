08cb25b08174c1db46112d5cb1308035ad8bfcdd
byObin <96936141+byObin@users.noreply.github.com>
2024-04-04 17:31:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,12 +1,8 @@
 n = int(input())
 
-# 5kg 봉지 최대 가능 수 부터 시작
-for i in range(n//5, -1, -1):
-    # 만약 5kg 봉지 최대 가능 수의 나머지가 3으로 나누어떨어지면
-    if (n - (i * 5)) % 3 == 0:
-        # 5kg 봉지 수 + 3kg 봉지 수의 합 출력
-        print(i + (n - (i * 5)) // 3)
+for i in range(n//5,-1,-1):
+    if (n-(i*5))%3 == 0:
+        print(i + (n-(i*5))//3)
         break
-# 루프를 마치고 나왔을 때 (5kg, 3kg으로 나누어떨어지지 않는 경우)
 else:
     print(-1)
