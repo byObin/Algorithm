98e9a5f1e2659e0fbc19f0767bd32a0d48aae1d2
byObin <96936141+byObin@users.noreply.github.com>
2024-04-05 08:12:24 +0900

    [Silver III] Title: 소수 구하기, Time: 104 ms, Memory: 38796 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,20 @@
-m, n = map(int, input().split())
+def find_prime(m, n):
+    if m <= 2:
+        print(2)
 
-for i in range(m, n+1):
-    if i == 1:
-        continue
-    for j in range(2, int(i ** 0.5) + 1):
-        if i % j == 0:
-            break
-    else:
-        print(i)
+    # 홀수 소수 찾기 위한 초기화, True = 소수 후보
+    primes = [False] + [True] * ((n - 1) // 2)
+
+    for k in range(1, int((n ** 0.5) / 2) + 1):
+        if primes[k]:
+            primes[2 * k * (k+1) :: k * 2 + 1] = [False] * (((n + 1) // 2 - k * k * 2) // (k * 2 + 1))
+
+    # m 이상 n 이하 소수 출력
+    for i, is_prime in enumerate(primes[m//2:], start = m//2):
+        if is_prime:
+            prime_num = 2*i + 1
+            if prime_num >= m:
+                print(prime_num)
+
+m, n = map(int, input().split())
+find_prime(m, n)
