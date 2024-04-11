f312f1b832213fc170ddd13a664f5ed134177ae5
byObin <byobin26@gmail.com>
2024-04-11 16:15:48 +0900

    shaker sort
    홀수 패스에서는 가장 작은 원소 맨 앞으로 이동,
    짝수 패스에서는 가장 큰 원소 맨 뒤로 이동
    
    패스의 스캔 방향 번갈아 바꾸어 버블정렬보다 적은 비교 횟수로 정렬 가능

--- exercise/sort/bubble_sort.py
+++ exercise/sort/bubble_sort.py
@@ -11,6 +11,24 @@ def bubble_sort(a: MutableSequence) -> None:
                 last = j
         k = last
 
+def shaker_sort(a: MutableSequence) -> None:
+    left = 0
+    right = len(a) - 1
+    last = right
+    while left < right:
+        for j in range(right, left, -1):
+            if a[j-1] > a[j]:
+                a[j-1], a[j] = a[j], a[j-1]
+                last = j
+        left = last
+
+    for j in range(left, right):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+            last = j
+    right = last
+
+
 if __name__ == '__main__':
     num = int(input('원소의 수 : '))
     x = [None] * num
@@ -18,6 +36,6 @@ if __name__ == '__main__':
     for i in range(num):
         x[i] = int(input(f'x[{i}] : '))
 
-    bubble_sort(x)
+    shaker_sort(x)
 
     print(x)
