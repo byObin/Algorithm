d2dff90a1762edb22e74c4d94d6e04c4acfa7ec2
byObin <96936141+byObin@users.noreply.github.com>
2024-04-20 17:25:19 +0900

    [level 2] Title: 타겟 넘버, Time: 3.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,21 +1,21 @@
-from collections import deque
+answer = 0
+
+def DFS(idx, numbers, target, value):
+# idx : 현재 숫자의 index, value : 현재까지 계산된 숫자의 합
+    global answer       # global 변수로 선언해 재귀 호출 간 값 유지/업데이트
+    n = len(numbers)
+    if idx == n and target == value:    # 모든 숫자 처리(idx == n) + 현재 값이 타겟과 일치(target == value)
+        answer += 1
+        return
+    if idx == n:                        # 모든 숫자 처리 후 타겟과 불일치하면 재귀호출 종료
+        return
+    
+    # 현재 idx 숫자를 value에 더하고/빼고 다음 숫자로 넘어감
+    DFS(idx+1, numbers, target, value+numbers[idx])
+    DFS(idx+1, numbers, target, value-numbers[idx])
 
 def solution(numbers, target):
-    answer = 0
-    q = deque()
-    n = len(numbers)
+    global answer
+    DFS(0, numbers, target, 0)
     
-    q.append([numbers[0], 0])
-    q.append([-1 * numbers[0], 0])
-
-    while q:
-        tmp, idx = q.popleft()  # q의 원소들 뽑아서 확인
-        idx += 1
-        
-        if idx < n:             # 아직 모든 원소 사용 x
-            q.append([tmp + numbers[idx], idx])
-            q.append([tmp - numbers[idx], idx])
-        else:                   # 모든 원소 사용 완료  
-            if tmp == target:   # 조합으로 target값 만들어졌는지 확인
-                answer += 1
     return answer
