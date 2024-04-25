03dffc6e924f8e4a073c0bad86120dd0f1335161
byObin <96936141+byObin@users.noreply.github.com>
2024-04-25 15:27:53 +0900

    [Gold V] Title: 치킨 배달, Time: 616 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,27 +1,34 @@
 from itertools import combinations
 
+# 입력
 n, m = map(int, input().split())
-
 town = [list(map(int, input().split())) for _ in range(n)]
+
+# 초기화
 house = []
 store = []
-
 chicken_distance = 9999
 
+# 집, 치킨집 위치 추출
 for i in range(n):
     for j in range(n):
         if town[i][j] == 1:
-            house.append([i, j])
+            house.append((i, j))
         elif town[i][j] == 2:
-            store.append([i, j])
+            store.append((i, j))
 
-for i in combinations(store, m):
+# 치킨집 조합별 최소 치킨 거리 계산
+for chicken_comb in combinations(store, m):
     tmp = 0
     for h in house:
-        c_dis = 999
-        for j in range(m):
-            c_dis = min(c_dis, abs(h[0] - i[j][0]) + abs(h[1] - i[j][1]))
-        tmp += c_dis
+        # 해당 집에서 가까운 치킨거리 계산
+        min_dis = 999
+        for c in chicken_comb:
+            dis = abs(h[0] - c[0]) + abs(h[1] - c[1])
+            min_dis = min(min_dis, dis)
+        tmp += min_dis
+
+    #결과값 업데이트
     chicken_distance = min(chicken_distance, tmp)
 
 print(chicken_distance)
