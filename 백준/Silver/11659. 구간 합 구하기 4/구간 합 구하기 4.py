12f6bab33948a7d03f4b94e6b61359029fa11502
byObin <96936141+byObin@users.noreply.github.com>
2024-04-27 20:44:01 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 220 ms, Memory: 71400 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,22 +1,19 @@
 import sys
 input = sys.stdin.read
-data = input().split()
+data = input().split()  # 한번에 입력 받기
 
-n, m = map(int, data[0:2])
+n, m = map(int, data[0:2]) 
 nums = list(map(int, data[2:n+2]))
 
-sums = [0] * (n+1)
+sums = [0] * (n+1)      # 누적합 저장할 sums 배열 초기화
 for i in range(1, n+1):
     sums[i] = sums[i-1] + nums[i-1]
-
-def get_sum(a, b):
-    return sums[b] - sums[a-1]
 
 index = n+2
 results = []
 for _ in range(m):
     a, b = map(int, data[index:index+2])
     index += 2
-    results.append(get_sum(a, b))
+    results.append(sums[b] - sums[a-1])
 
-print("\n".join(map(str, results)))
+print("\n".join(map(str, results))) # results 값들 한줄씩 출력
