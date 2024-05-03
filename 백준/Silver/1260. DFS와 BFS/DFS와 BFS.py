f564b9c6db84324519b7f74faade5519425ffd78
byObin <96936141+byObin@users.noreply.github.com>
2024-05-03 14:07:30 +0900

    [Silver II] Title: DFS와 BFS, Time: 84 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,4 +1,7 @@
 from collections import deque
+import sys
+
+input = sys.stdin.readline
 
 def recursive_dfs(graph, v, visited, result):
     visited[v] = True
@@ -30,7 +33,7 @@ for _ in range(m):
     graph[s].append(e)
     graph[e].append(s)
 
-visited_d = [False] * (n+1)
+visited_d= [False] * (n+1)
 visited_b = [False] * (n+1)
 result = []
 
