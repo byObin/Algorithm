342386764b4ac5d584092cb88276bac8ffa72a02
byObin <96936141+byObin@users.noreply.github.com>
2024-05-13 16:13:38 +0900

    [Silver II] Title: 섬의 개수, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -6,13 +6,17 @@ def dfs(i, j):
     # 상하좌우, 대각선 방향벡터
     dx = [1, -1, 0, 0, 1, 1, -1, -1]
     dy = [0, 0, 1, -1, 1, -1, 1, -1]
-
+    stack = [(i, j)]
     graph[i][j] = 0
-    for n in range(8):
-        nx = i + dx[n]
-        ny = j + dy[n]
-        if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
-            dfs(nx, ny)
+
+    while stack:
+        x, y = stack.pop()
+        for n in range(8):
+            nx = x + dx[n]
+            ny = y + dy[n]
+            if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                stack.append((nx, ny))
 
 while True:
     w, h = map(int, input().split())
