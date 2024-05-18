54e699b0519ac1d7bf99a5973481c44566ddf7bf
byObin <96936141+byObin@users.noreply.github.com>
2024-05-18 16:07:08 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 516 ms, Memory: 60324 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -2,27 +2,33 @@ import sys
 sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-def dfs(r,cnt):
-    visited[r] = True
-    order[r] = cnt
-    cnt += 1
-    for w in sorted(graph[r]):
-        if not visited[w]:
-            cnt = dfs(w, cnt)
-    return cnt
+def dfs_stack(start):
+    stack = [start]
+    order = [0] * (n + 1)
+    cnt = 1
+
+    while stack:
+        s = stack.pop()
+        if not visited[s]:
+            visited[s] = True
+            order[s] = cnt
+            cnt += 1
+            for w in sorted(graph[s], reverse=True):
+                if not visited[w]:
+                    stack.append(w)
+    return order
 
 n, m, r = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
-order = [0] * (n+1)
-cnt = 1
+
 
 for _ in range(m):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-dfs(r, cnt)
+order = dfs_stack(r)
 
 for i in range(1, n+1):
     print(order[i])
