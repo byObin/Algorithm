e2a9f6e91504dd7846223a8b0f5c5b46deb8d3a3
byObin <96936141+byObin@users.noreply.github.com>
2024-05-18 19:48:14 +0900

    [Silver IV] Title: 바닥 장식, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,17 +1,22 @@
 d = [-1, 1]
 
-def dfs(i, j):
-    visited[i][j] = True
-    if ground[i][j] == '-':
-        for y in range(2):
-            ny = j + d[y]
-            if 0 <= ny < m and not visited[i][ny] and ground[i][ny] == ground[i][j]:
-                dfs(i, ny)
-    else:
-        for x in range(2):
-            nx = i + d[x]
-            if 0 <= nx < n and not visited[nx][j] and ground[nx][j] == ground[i][j]:
-                dfs(nx, j)
+def dfs_stack(i, j):
+    stack = [(i, j)]
+    while stack:
+        x, y = stack.pop()
+        if not visited[x][y]:
+            visited[x][y] = True
+
+            if ground[x][y] == '-':
+                for dy in d:
+                    ny = y + dy
+                    if 0 <= ny < m and not visited[x][ny] and ground[x][ny] == ground[x][y]:
+                        stack.append((x, ny))
+            else:
+                for dx in d:
+                    nx = x + dx
+                    if 0 <= nx < n and not visited[nx][y] and ground[nx][y] == ground[x][y]:
+                        stack.append((nx, y))
 
 n, m = map(int, input().split())
 ground = [list(input()) for _ in range(n)]
@@ -20,6 +25,6 @@ cnt = 0
 for i in range(n):
     for j in range(m):
         if not visited[i][j]:
-            dfs(i, j)
+            dfs_stack(i, j)
             cnt += 1
 print(cnt)
