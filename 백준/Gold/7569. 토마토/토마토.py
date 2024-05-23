17f7bd04372332bf0971ed458b0e16cc8e0b8e0e
byObin <96936141+byObin@users.noreply.github.com>
2024-05-23 23:03:06 +0900

    [Gold V] Title: 토마토, Time: 1668 ms, Memory: 48508 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,50 +1,42 @@
-import sys
 from collections import deque
-
-m, n, h = map(int, input().split())
-
-matrix = [[list(map(int, sys.stdin.readline().split())) for _ in range(n)] for _ in range(h)]
-visited = [[[False]*m for _ in range(n)] for _ in range(h)]
-
-queue = deque()
-
-dx = [-1,1,0,0,0,0]
-dy = [0,0,-1,1,0,0]
-dz = [0,0,0,0,-1,1]
-
-answer = 0
+import sys
 
 def bfs():
     while queue:
-        x,y,z = queue.popleft()
+        x, y, z = queue.popleft()
+        for dx, dy, dz in [(-1,0,0),(1,0,0),(0,-1,0),(0,1,0),(0,0,-1),(0,0,1)]:
+            nx, ny, nz = x + dx, y + dy, z + dz
+            if 0 <= nx < h and 0 <= ny < n and 0 <= nz < m and graph[nx][ny][nz] == 0:
+                graph[nx][ny][nz] = graph[x][y][z] + 1
+                queue.append((nx, ny, nz))
 
-        for i in range(6):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            nz = z + dz[i]
+def calculate_days():
+    bfs()
 
-            if nx < 0 or nx >= h or ny < 0 or ny >= n or nz < 0 or nz >= m:
-                continue
+    max_days = 0
 
-            if matrix[nx][ny][nz] == 0 and visited[nx][ny][nz] == False:
-                queue.append((nx,ny,nz))
-                matrix[nx][ny][nz] = matrix[x][y][z] + 1
-                visited[nx][ny][nz] = True
+    for i in range(h):
+        for j in range(n):
+            for k in range(m):
+                if graph[i][j][k] == 0:
+                    return -1
+                max_days = max(max_days, graph[i][j][k])
+    
+    return max_days - 1
 
-for a in range(h):
-    for b in range(n):
-        for c in range(m):
-            if matrix[a][b][c] == 1 and visited[a][b][c] == 0:
-                queue.append((a,b,c))
-                visited[a][b][c] = True
-bfs()
+m, n, h = map(int, input().split())
+graph = []
+queue = deque([])
 
-for a in matrix:
-    for b in a:
-        for c in b:
-            if c == 0:
-                print(-1)
-                exit(0)            
-        answer = max(answer, max(b))
+for i in range(h):
+    tmp = []
+    for j in range(n):
+        tmp.append(list(map(int, sys.stdin.readline().strip().split())))
+        for k in range(m):
+            if tmp[j][k] == 1:
+                queue.append([i, j, k])
+    graph.append(tmp)
 
-print(answer-1)
+result = calculate_days()
+
+print(result)
