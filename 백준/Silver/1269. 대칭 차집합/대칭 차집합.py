14892919eb601e09f3733f3585c8f5f1124913b9
byObin <96936141+byObin@users.noreply.github.com>
2024-06-05 23:36:04 +0900

    [Silver IV] Title: 대칭 차집합, Time: 240 ms, Memory: 81352 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -3,4 +3,6 @@ a, b = map(int, input().split())
 set_a = set(map(int, input().split()))
 set_b = set(map(int, input().split()))
 
-print(len(set_a ^ set_b))
+both = len(set_a & set_b)
+
+print(a + b - (both * 2))
