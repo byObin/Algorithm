a6cd76067a3230beacfd90308168178da6577cb8
byObin <96936141+byObin@users.noreply.github.com>
2024-06-06 16:22:35 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 544 ms, Memory: 240712 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,10 +1,8 @@
-from itertools import combinations
-
 word = input()
 part_str = set()
 
 for start in range(len(word)):
-    for end in range(len(word)+1):
+    for end in range(start+1, len(word)+1):
         part_str.add(word[start:end])
 
-print(len(part_str) - 1)
+print(len(part_str))
