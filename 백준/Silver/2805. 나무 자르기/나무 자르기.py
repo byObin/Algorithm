ba36e6a85eeeaa254059f408834478bffd24178e
byObin <96936141+byObin@users.noreply.github.com>
2024-06-11 08:30:44 +0900

    [Silver II] Title: 나무 자르기, Time: 4800 ms, Memory: 143316 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+trees = list(map(int, input().split()))
+
+start, end = 1, max(trees)
+
+while start <= end:
+    mid = (start + end) // 2
+
+    log_sum = 0     # 현재 높이에서 벌목 나무 합
+    for i in trees:
+        if i >= mid:
+            log_sum += i - mid
+    
+    if log_sum >= m:
+        start = mid + 1
+    else:
+        end = mid -1
+
+print(end)
