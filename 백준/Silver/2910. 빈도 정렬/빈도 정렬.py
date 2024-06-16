a15ebb5100579318fd20cdfce072c04dcf03fc6f
byObin <96936141+byObin@users.noreply.github.com>
2024-06-17 08:04:31 +0900

    [Silver III] Title: 빈도 정렬, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,13 +1,23 @@
-from collections import defaultdict
-
 n, c = map(int, input().split())
 nums = list(map(int, input().split()))
-dic = defaultdict(int)
+
+dic = {}
+order = 0
+
+# dic[숫자] = [빈도, 출현순서] 형태로 저장
 for i in nums:
-    dic[i] += 1
+    if i in dic:
+        dic[i][0] += 1
+    else:
+        dic[i] = [1, order]
+        order += 1
+# 출현빈도(내림차순), 출현순서(오름차순) 순으로 정렬
+sorted_dic = sorted(dic.items(), key = lambda x: (-x[1][0], x[1][1]))
 
-sorted_dic = sorted(dic.items(), key = lambda x: (-x[1], list(dic.keys()).index(x[0])))
+result = []
+# 출현횟수만큼 result에 추가(append 반복보다 빠름)
+for num, (cnt, _) in sorted_dic:
+    result.extend([num] * cnt)
 
-for i, j in sorted_dic:
-    for _ in range(j):
-        print(i, end=' ')
+# * : 리스트 요소 개별 인자로 출력
+print(*result)
