63ed49dccddd21f056020cd501cd6d7e648d9bfa
byObin <96936141+byObin@users.noreply.github.com>
2024-07-12 15:47:35 +0900

    [Bronze II] Title: 단어의 개수, Time: 12 ms, Memory: 1968 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	char S[1000000];
+	gets(S);
+	int count = 0;
+	int k = 0;
+	if (S[0] != ' ') {
+		count++;
+	}
+	while (S[k]) {
+		if (S[k] == ' ' && S[k+1]!='\0') {
+			count++;
+		}
+		k++;
+	}
+	printf("%d", count);
+}
