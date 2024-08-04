a661c01702ae7814b0e9144c8eda25f186529d5b
byObin <96936141+byObin@users.noreply.github.com>
2024-08-04 17:47:11 +0900

    [Silver I] Title: 그림, Time: 308 ms, Memory: 35576 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,33 +1,42 @@
 import sys
-sys.setrecursionlimit(10 ** 7)
+from collections import deque
+
+input = sys.stdin.readline
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs(x, y):
+    q = deque([(x, y)])
     visited[x][y] = True
-    global size
-    for nx, ny in zip(dx, dy):
-        cx = x + nx
-        cy = y + ny
-        if 0 <= cx < n and 0 <= cy < m and graph[cx][cy] == 1 and not visited[cx][cy]:
-           bfs(cx, cy)
-           size += 1
+    size = 1
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny <m and graph[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny))
+                size += 1
     return size
 
+def main():
+    global n, m, graph, visited
+    n, m = map(int, input().split())
+    graph = [list(map(int, input().split())) for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+    cnt = 0
+    max_size = 0
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
-cnt = 0
-max_size = 0
-size = 1
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1 and not visited[i][j]:
-            cnt += 1
-            size = 1
-            max_size = max(max_size, bfs(i, j))
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1 and not visited[i][j]:
+                cnt += 1
+                max_size = max(max_size, bfs(i, j))
 
-print(cnt)
-print(max_size)
+    print(cnt)
+    print(max_size)
+
+if __name__ == "__main__":
+    main()
