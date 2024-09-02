b19f483298ae84576c99bc6f31b28ffa7abe1e87
byObin <96936141+byObin@users.noreply.github.com>
2024-09-02 22:56:55 +0900

    [Silver III] Title: 두 수의 합, Time: 84 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -2,13 +2,20 @@ n = int(input())
 arr = list(map(int, input().split()))
 x = int(input())
 
-arr2 = [0] * 2000000
-for i in range(n):
-    arr2[arr[i]] = i
-
+# 배열 정렬 후 포인터 양쪽 끝에 두 개 설정
+arr.sort()
+left, right = 0, n - 1
 cnt = 0
-for i in range(n):
-    if arr2[x - arr[i]] > i:
+
+while left < right:
+    current_sum = arr[left] + arr[right]
+    if current_sum == x:
         cnt += 1
+        left += 1
+        right -= 1
+    elif current_sum < x:
+        left += 1
+    else:
+        right -= 1
 
 print(cnt)
