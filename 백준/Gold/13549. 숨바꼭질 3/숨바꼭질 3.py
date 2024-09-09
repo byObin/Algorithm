9d7bdf15beb775cb29fe95979daad3739130ac18
byObin <96936141+byObin@users.noreply.github.com>
2024-09-09 11:23:38 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 84 ms, Memory: 35220 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,36 +1,32 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
- 
-a,b = map(int,input().split())
-limit = 100001
-time = [0]*limit
- 
-def bfs(x,y):
-    q = deque()
-    if x == 0 :
-        q.append(1)
-    else :
-        q.append(x)
-    
+
+def bfs(now, target):
+    q = deque([now])
+    time[now] = 0   # 방문 처리
+
     while q:
         x = q.popleft()
-        if y == x :
+        # 현재 위치가 target(동생 위치)와 일치하면 시간 리턴
+        if x == target:
             return time[x]
         
         for nx in (x-1,x+1,x*2):
-            if 0 <= nx < limit and time[nx]==0:
-                if nx == 2*x :
+            if 0<= nx < limit and time[nx] == -1:
+                if nx == 2*x:
                     time[nx] = time[x]
-                    q.appendleft(nx)
-                else : 
-                    time[nx] = time[x] + 1
+                    # 시간 증가하지 않았으므로 q 맨 앞 추가
+                    q.appendleft(nx)    
+                else:
+                    # 시간 1 증가, q 맨 뒤에 추가
+                    time[nx] = time[x] +1
                     q.append(nx)
- 
-if a==0:
-    if b==0:
-        print(0)
-    else:
-        print(bfs(a,b)+1)
-else :
-    print(bfs(a,b))
+
+
+# 현재 위치 p, 목표 위치 t 입력 받기
+p, t = map(int, input().split())
+limit = 100001
+time = [-1] * limit     # 방문 체크 및 시간 저장
+
+print(bfs(p, t))
