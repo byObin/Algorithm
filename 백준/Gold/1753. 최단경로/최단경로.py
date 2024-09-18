cc307663e58b7f05e42ed043bb08ccd78031137c
byObin <96936141+byObin@users.noreply.github.com>
2024-09-18 23:32:37 +0900

    [Gold IV] Title: 최단경로, Time: 608 ms, Memory: 68336 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -4,15 +4,19 @@ input = sys.stdin.readline
 
 def dijkstra(s, dis):
     dis[s] = 0
+    q = []
     heapq.heappush(q, (0, s))
     while q:
         cur_dis, now = heapq.heappop(q)
+        if cur_dis > dis[now]:
+            continue
         for n, weight in graph[now]:
             cost = cur_dis + weight
-            if dis[n] == -1 or cost < dis[n]:
+            if cost < dis[n]:
                 dis[n] = cost
                 heapq.heappush(q, (cost, n))
 
+                
 V, E = map(int, input().split())
 K = int(input())
 
@@ -22,15 +26,15 @@ for _ in range(E):
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
 
-# -1로 초기화해 최단경로 모르는 상태 표시
-dis = [-1] * (V+1)
-q = []
+# 최단경로 탐색 전 무한대(inf)로 초기화
+dis = [float('inf')] * (V+1)
 
 dijkstra(K, dis)
 
 for i in dis[1:]:
-    # i가 -1일 경우 최단경로 탐색되지 않음 = 경로 존재 x
-    if i != -1:
+    if i != float('inf'):
         print(i)
+
+    # i가 무한대인 경우 경로 존재 x
     else:
         print('INF')
